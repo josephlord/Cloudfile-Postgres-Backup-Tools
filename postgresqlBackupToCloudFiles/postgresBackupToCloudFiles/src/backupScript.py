08012fe873d59ccbf398c730be465d1a31bdd256
josephl <joseph@humanfriendly.com>
2011-09-08 00:34:55 +0100

    Minor early progress.

--- postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
+++ postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
@@ -1,6 +1,31 @@
-#!/bin/python
+#!/usr/bin/python
 
-#import "python-cloudfiles.py"
+import cloudfiles
+from myConfig import *
+# myConfig should be a modified copy of configTemplate.py with your settings in it.
+from datetime import datetime
+import os
 
-print "Import didn't fail."
 
+startime = datetime.now()
+
+os.chdir(TMPDIR)
+commandline = 'pg_dump -w -h localhost -U ' + DBUSER + ' -f ' + new.dump + ' ' + DBNAME#postgres -w BIVstats
+backupResult = subprocess.check_output(commandline)
+
+
+cloudFConnection = cloudfiles.Connection(CLOUDFILEUSER, CLOUDFILESAPIKEY, authurl=CLOUDFILESAUTHHOST)
+
+if CLOUDFILEDESTINATIONCONTAINERADDMONTHANDYEAR:
+    containerName = str(startime.year) + '-' + str(startime.month) + '-' + CLOUDFILEDESTINATIONCONTAINER
+else:    
+    containerName = CLOUDFILEDESTINATIONCONTAINER
+try:
+    containers = cloudFConnection.get_containers(containerName)
+except cloudfiles.errors.NoSuchContainer:
+    cloudFConnection.create_container('containerName')
+
+
+    
+    
+print containers
---
+++ postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/configTemplate.py
@@ -0,0 +1,12 @@
+
+CLOUDFILEUSER = 'cloudhostusername'
+CLOUDFILESAPIKEY = '1234567890'
+CLOUDFILESAUTHHOST = "https://lon.auth.api.rackspacecloud.com/v1.0"
+CLOUDFILEDESTINATIONCONTAINER = 'postgresBackups'
+CLOUDFILEDESTINATIONCONTAINERADDMONTHANDYEAR = True
+TMPDIR = 'c:\DatabaseFiles\automatedBackup'
+DBNAME = 'database name'
+DBUSER = 'dbusername'
+DBHOST = 'dbhostname'
+# If there is a password you must have the .pgpass file (or on Windows the %APPDATA%\postgresql\pgpass.conf) file configured in your home directory
+# http://www.postgresql.org/docs/9.0/static/libpq-pgpass.html
