95f0e90893b6638bfd068af27614be02d142ec8b
Joseph Lord <joseph@human-friendly.com>
2011-09-08 11:23:48 +0100

    Added readme and some import error handling to give slightly friendlier messages.

--- postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
+++ postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
@@ -1,14 +1,51 @@
 #!/usr/bin/python
 
-import cloudfiles
-from myConfig import *
+
+#    This is a simple program to backup from a PostgreSQL database to
+#    Rackspace's Cloud Files
+#    Copyright (C) 2011  Human Friendly Ltd.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+importfailed = False
+try:
+    import cloudfiles
+except:
+    print """You need to install the python-cloudfiles library from Rackspace"""
+    importfailed = True
+try:
+    from myConfig import *
+except:
+    print """Copy the configTemplate.py file to myConfig.py and set all items 
+    according to your settings.  Make sure the file is in the same folder as the
+    backupScript.py or otherwise on your PYTHONPATH"""
+    importfailed = True
 # myConfig should be a modified copy of configTemplate.py with your settings in it.
-from datetime import datetime
-import os
-import subprocess
-from subprocess import CalledProcessError
 
+try:
+    from datetime import datetime
+    import os
+    import subprocess
+    from subprocess import CalledProcessError
+except:
+    print """Standard library import error.  Check that you have Python 2.7 or 
+    later and that the PYTHONPATH is properly configured."""
+    importfailed = True
 
+if importfailed:
+    exit()
 
 startime = datetime.now()
 
