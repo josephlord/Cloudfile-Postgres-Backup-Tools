65bb7a601108d215f9143af3362db1c33b058a3c
josephl <joseph@humanfriendly.com>
2011-09-08 10:15:11 +0100

    Runs - still not very advanced.

--- postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
+++ postgresqlBackupToCloudFiles/postgresBackupToCloudFiles/src/backupScript.py
@@ -5,27 +5,44 @@ from myConfig import *
 # myConfig should be a modified copy of configTemplate.py with your settings in it.
 from datetime import datetime
 import os
+import subprocess
+from subprocess import CalledProcessError
+
 
 
 startime = datetime.now()
 
 os.chdir(TMPDIR)
-commandline = 'pg_dump -w -h localhost -U ' + DBUSER + ' -f ' + new.dump + ' ' + DBNAME#postgres -w BIVstats
-backupResult = subprocess.check_output(commandline)
+#commandline = ['/usr/bin/pg_dump', '-w', '-h localhost', '-U' , DBUSER,
+#               ' -f tmp.dump ', DBNAME]#postgres -w BIVstats
+commandline = ['pg_dump', '-w', '-h', 'localhost', '-U', DBUSER, '-f',
+               'tmp.dump', DBNAME]
 
+print commandline
+try:
+    backupResult =  subprocess.check_output(commandline,
+                                            stderr=subprocess.STDOUT)
+except CalledProcessError as e:
+    print e
+    print e.output
+    exit()
+print backupResult
 
-cloudFConnection = cloudfiles.Connection(CLOUDFILEUSER, CLOUDFILESAPIKEY, authurl=CLOUDFILESAUTHHOST)
+cloudFConnection = cloudfiles.Connection(CLOUDFILEUSER, CLOUDFILESAPIKEY, 
+                                         authurl=CLOUDFILESAUTHHOST)
 
 if CLOUDFILEDESTINATIONCONTAINERADDMONTHANDYEAR:
-    containerName = str(startime.year) + '-' + str(startime.month) + '-' + CLOUDFILEDESTINATIONCONTAINER
+    containerName = str(startime.year) + '-' + str(startime.month) + '-' 
+    containerName = containerName + CLOUDFILEDESTINATIONCONTAINER
 else:    
     containerName = CLOUDFILEDESTINATIONCONTAINER
 try:
-    containers = cloudFConnection.get_containers(containerName)
+    container = cloudFConnection.get_container(containerName)
 except cloudfiles.errors.NoSuchContainer:
-    cloudFConnection.create_container('containerName')
+    container = cloudFConnection.create_container(containerName)
 
+filesavename = str(startime.year) + str(startime.month) + str(startime.day) 
+filesavename = filesavename + DBNAME + '-backup.dump'
 
-    
-    
-print containers
+remoteObjust = container.create_object(filesavename)
+remoteObjust.load_from_filename('tmp.dump', verify=True)
